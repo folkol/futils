7402fb83b0b27a61c942ebb9050e93f065feb3b0
Matte <mattias.johansson@ivbar.com>
2019-09-26 10:12:25 +0200

    Added ifmain

--- bin/groupby.py
+++ bin/groupby.py
@@ -4,26 +4,34 @@ import sys
 from collections import defaultdict
 import re
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--field', type=int, default=0)
-parser.add_argument('-d', '--delimiter', default=r'\s+')
-parser.add_argument('--humanize', action='store_true')
-args = parser.parse_args()
 
-keysize = 0
-groups = defaultdict(list)
-for line in sys.stdin:  # TODO Find out how to pair argparse with fileinput.input
-	fields = re.split(args.delimiter, line.rstrip('\n'))
-	key = fields[args.field]
-	if len(key) > keysize:
-		keysize = len(key)
-	groups[key].append(' '.join(f for i, f in enumerate(fields) if i != args.field))
+def main(field, delimiter):
+    keysize = 0
+    groups = defaultdict(list)
+    # TODO Find out how to pair argparse with fileinput.input
+    for line in sys.stdin:
+        fields = re.split(delimiter, line.rstrip('\n'))
+        key = fields[field]
+        if len(key) > keysize:
+            keysize = len(key)
+        fields = (f for i, f in enumerate(fields) if i != field)
+        groups[key].append(' '.join(fields))
 
-for key, group in groups.items():
-	if args.humanize:
-		print(f'{key}:')
-		for value in sorted(group, key=str.lower):
-			print(f' - {value}')
-	else:
-		for value in sorted(group):
-			print(f'{key}\t{value}')
+    for key in sorted(groups, key=str.lower):
+        if args.humanize:
+            print(f'{key}:')
+            for value in sorted(groups[key], key=str.lower):
+                print(f' - {value}')
+        else:
+            for value in sorted(groups[key]):
+                print(f'{key}\t{value}')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--field', type=int, default=1)
+    parser.add_argument('-d', '--delimiter', default=r'\s+')
+    parser.add_argument('--humanize', action='store_true')
+    args = parser.parse_args()
+
+    main(args.field - 1, args.delimiter)
