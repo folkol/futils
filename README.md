c035e8c5138b20421389b9c1459dfcadb7d5d333
Matte <mattias4@kth.se>
2020-08-10 19:45:45 +0200

    Now using futils as a meta-package for various futils-foo utils

--- README.md
+++ README.md
@@ -2,21 +2,4 @@
 
 ## Installation
 
-Add the `bin` directory inside this repo to your `$PATH`.
-
-## Build
-
-Some tools needs to be built from source:
-
-```BASH
-$ brew install cmake
-$ cmake .
-$ make
-$ make install
-```
-
-### lorem generator
-
-The lorem generator needs a database of state transitions in ~/.lorem. If you
-do not have a body of text to generate one from, feel free to use the sample
-database in this repo. (Generated from The Call of Cthulu.)
+`$ pip install folkol-utils`
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='folkol-utils',
+    version='0.0.2',
+    description='meta-package for various command line utilities (futils-xxx)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/folkol/futils',
+    author='folkol',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Terminals',
+        'Topic :: Text Processing :: Filters',
+        'License :: OSI Approved :: MIT License',
+    ],
+    install_requires=[
+        'futils-grab'
+    ],
+    keywords='shell, unix filter',
+    python_requires='>=3.6, <4',
+    project_urls={
+        'Bug Reports': 'https://github.com/folkol/futils/issues',
+        'Source': 'https://github.com/folkol/futils',
+    },
+)
