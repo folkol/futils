4fad0cdd860cffce735474722f7950b95f8829c2
Matte <mattias.johansson@funnel.io>
2023-07-23 01:10:58 +0200

    Fixed indentation bug

--- align/src/main.rs
+++ align/src/main.rs
@@ -21,8 +21,9 @@ fn main() {
             )
         })
         .collect();
+    dbg!(&lines);
     let max_indent = &lines.iter().map(|(x, _)| *x).max().unwrap_or(0);
     for (i, line) in lines {
-        println!("{:<foo$}  {line:>foo$}", "", foo = (max_indent - i))
+        println!("{:<foo$}  {line}", "", foo = (max_indent - i))
     }
 }
