0a63381445f4932b2bbe1328da0945465756e2cf
Matte <mattias4@kth.se>
2024-09-07 13:05:43 +0200

    Removed printouts

--- align/src/main.rs
+++ align/src/main.rs
@@ -8,7 +8,6 @@ fn main() {
     let pattern: Regex = match args().nth(1) {
         None => panic!("usage: align PATTERN"),
         Some(pattern) => {
-            println!("pattern={pattern}");
             Regex::new(&pattern).unwrap()
         }
     };
@@ -19,14 +18,11 @@ fn main() {
         .map(|line| {
             let i = match pattern.find(&line) {
                 None => {
-                    println!("no match");
                     0
                 }
                 Some(m) => {
-                    println!("match, i={}", m.start());
                     for (grapheme_offset, (i, _)) in line.grapheme_indices(true).enumerate() {
                         if i == m.start() {
-                            println!("Found byte offset of grapheme cluster");
                             return (grapheme_offset, line);
                         }
                     }
@@ -37,9 +33,7 @@ fn main() {
         })
         .collect();
     let max_indent = &lines.iter().map(|(x, _)| *x).max().unwrap_or(0);
-    println!("max_indent={max_indent}");
     for (i, line) in lines {
-        println!("i={i} line={line}");
         println!("{:<foo$}  {line}", "", foo = max_indent - i)
     }
 }
