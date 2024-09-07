2f3e8988b27a10bbbce1c89241148bffb8266fbd
Matte <mattias4@kth.se>
2024-09-07 13:03:49 +0200

    Now indenting with 'unicode grapheme cluster index' instead of byte index in order to support Swedish chars

--- align/src/main.rs
+++ align/src/main.rs
@@ -1,28 +1,45 @@
 use std::env::args;
 use std::io::stdin;
+use unicode_segmentation::{GraphemeCursor, UnicodeSegmentation};
 
 use regex::Regex;
 
 fn main() {
     let pattern: Regex = match args().nth(1) {
         None => panic!("usage: align PATTERN"),
-        Some(pattern) => Regex::new(&pattern).unwrap(),
+        Some(pattern) => {
+            println!("pattern={pattern}");
+            Regex::new(&pattern).unwrap()
+        }
     };
+    println!("regex={:?}", pattern);
     let lines: Vec<(usize, String)> = stdin()
         .lines()
         .map_while(Result::ok)
         .map(|line| {
-            (
-                match pattern.find(&line) {
-                    None => 0,
-                    Some(m) => m.start(),
-                },
-                line,
-            )
+            let i = match pattern.find(&line) {
+                None => {
+                    println!("no match");
+                    0
+                }
+                Some(m) => {
+                    println!("match, i={}", m.start());
+                    for (grapheme_offset, (i, _)) in line.grapheme_indices(true).enumerate() {
+                        if i == m.start() {
+                            println!("Found byte offset of grapheme cluster");
+                            return (grapheme_offset, line);
+                        }
+                    }
+                    m.start()
+                }
+            };
+            (i, line)
         })
         .collect();
     let max_indent = &lines.iter().map(|(x, _)| *x).max().unwrap_or(0);
+    println!("max_indent={max_indent}");
     for (i, line) in lines {
-        println!("{:<foo$}  {line}", "", foo = (max_indent - i))
+        println!("i={i} line={line}");
+        println!("{:<foo$}  {line}", "", foo = max_indent - i)
     }
 }
