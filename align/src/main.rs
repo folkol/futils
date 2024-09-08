7d57338203e6adfd404dad0b380267e3151a08d3
Matte <mattias4@kth.se>
2024-09-08 16:23:48 +0200

    Fixed alignment and some coloring

--- align/src/main.rs
+++ align/src/main.rs
@@ -1,39 +1,49 @@
+use colored::Colorize;
 use std::env::args;
 use std::io::stdin;
+
+use regex::{Regex, RegexBuilder};
 use unicode_segmentation::UnicodeSegmentation;
 
-use regex::Regex;
-
 fn main() {
-    let pattern: Regex = match args().nth(1) {
+    let regex: Regex = match args().nth(1) {
         None => panic!("usage: align PATTERN"),
         Some(pattern) => {
-            Regex::new(&pattern).unwrap()
+            RegexBuilder::new(&pattern)
+                .unicode(true)
+                .case_insensitive(true)
+                .build().unwrap()
         }
     };
-    println!("regex={:?}", pattern);
-    let lines: Vec<(usize, String)> = stdin()
+
+    let mut max_indent = 0;
+    let lines: Vec<(usize, usize, usize, String)> = stdin()
         .lines()
         .map_while(Result::ok)
-        .map(|line| {
-            let i = match pattern.find(&line) {
-                None => {
-                    0
-                }
-                Some(m) => {
-                    for (grapheme_offset, (i, _)) in line.grapheme_indices(true).enumerate() {
+        .filter_map(|line| {
+            regex.find(&line).map(|m| {
+                let my_line = line.clone();
+                let (indent, begin, end) = 'block: {
+                    let grapheme_indices = line.grapheme_indices(true).enumerate().collect::<Vec<_>>();
+                    for (indent, (i, _)) in grapheme_indices.into_iter() {
                         if i == m.start() {
-                            return (grapheme_offset, line);
+                            max_indent = max_indent.max(indent);
+                            break 'block (indent, m.start(), m.end());
                         }
                     }
-                    m.start()
-                }
-            };
-            (i, line)
+                    (0, 0, 0)
+                };
+                (indent, begin, end, my_line)
+            })
         })
         .collect();
-    let max_indent = &lines.iter().map(|(x, _)| *x).max().unwrap_or(0);
-    for (i, line) in lines {
-        println!("{:<foo$}  {line}", "", foo = max_indent - i)
+    for (indent, begin, end, line) in lines {
+        println!(
+            "{:<indent$}  {}{}{}",
+            "",
+            &line[..begin],
+            &line[begin..end].green().bold(),
+            &line[end..],
+            indent = max_indent - indent)
     }
 }
